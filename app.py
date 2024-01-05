c0955b9aecae7e18137a682b418581a95f0b832b
Paweł Winkler <147107563+Wajchaa@users.noreply.github.com>
2024-01-05 22:14:46 +0100

    gallery login requred

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, render_template, request
 from flask import Flask, render_template
 from config import app, db, bcrypt, User
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 from urllib.parse import urlsplit
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -276,8 +276,8 @@ def planet_masses_chart():
     return render_template('planet-masses.html', planets_masses_data=planets_masses_data)
 
   
-@app.route('/register', methods=['GET', 'POST'])
-def register():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     # Creating a new user when the register form validates
     if forms.RegistrationForm().validate_on_submit():
         # Creating a new user in the database
@@ -294,20 +294,20 @@ def register():
         if user and bcrypt.check_password_hash(user.password, forms.RegistrationForm().password.data):
             login_user(user)
             # Taking the user to the authenticated side of the site
-            return redirect(url_for('register'))
+            return redirect(url_for('login'))
 
     if forms.LoginForm().validate_on_submit():
         user = User.query.filter_by(email=forms.LoginForm().email.data).first()
         if user and bcrypt.check_password_hash(user.password, forms.LoginForm().password.data):
             login_user(user, remember=forms.LoginForm().remember.data)
 
-            return redirect(url_for('register'))
+            return redirect(url_for('login'))
 
     if (request.method == "POST") & (request.form.get('post_header') == 'log out'):
         logout_user()
-        return redirect(url_for('register'))
+        return redirect(url_for('login'))
 
-    return render_template('register.html',
+    return render_template('login.html',
                            login_form=forms.LoginForm(),
                            register_form=forms.RegistrationForm())
 
@@ -520,17 +520,8 @@ def uranus():
 def neptune():
     return render_template('Neptune.html')
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get('username') is None or session.get('if_logged') is None:
-            return redirect('/register',code=302)
-        next_page = request.args.get('next')
-        if not next_page or urlsplit(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return decorated_function
 @app.route('/gallery' , methods=['GET', 'POST'])
+@login_required
 def gallery():
     return render_template('gallery.html')
 if __name__ == "__main__":
