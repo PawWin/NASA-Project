958aedd7393c373418a660c8f38738aeaf628cf1
Balon02 <147401028+Balon02@users.noreply.github.com>
2024-01-05 21:33:03 +0100

    added existing email error exception handlind and image table to database

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ from datetime import datetime, timedelta
 
 from flask import Flask, render_template, request
 from flask import Flask, render_template
+from sqlalchemy.exc import IntegrityError
+
 from config import app, db, bcrypt, User
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import login_user, current_user, logout_user, login_required
+from sqlite3 import IntegrityError
+from sqlalchemy.exc import IntegrityError
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -283,9 +287,11 @@ def register():
         user = User(username=register_form.username.data,
                     email=register_form.email.data,
                     password=hashed_password)
-
-        db.session.add(user)
-        db.session.commit()
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except IntegrityError:
+            return redirect(url_for('register'))
         # Signing in the user after creating them
         user = User.query.filter_by(email=forms.RegistrationForm().email.data).first()
         if user and bcrypt.check_password_hash(user.password, forms.RegistrationForm().password.data):
--- config.py
+++ config.py
@@ -35,3 +35,13 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique = True, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
     password = db.Column(db.String(60), nullable = False)
+
+    images = db.relationship('Image', backref='user', lazy=True)
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    image_link = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"Image('{self.image_link}')"
